30634143b113bdb4c7a0fe117c5e3bb22d606211
Yann Picard <yann.picard95@gmail.com>
2019-06-14 15:12:24 +0200

    Improve run.sh script

--- run.sh
+++ run.sh
@@ -1,1 +1,7 @@
-docker run -d -v /etc/letsencrypt/live/ypicard.dev/fullchain.pem:/etc/letsencrypt/live/ypicard.dev/fullchain.pem -v /etc/letsencrypt/live/ypicard.dev/privkey.pem:/etc/letsencrypt/live/ypicard.dev/privkey.pem -p80:8080 -p 443:8443 -t portfolio-yann
+# stop and remove all docker containers
+docker stop $(docker ps -a -q)
+docker rm $(docker ps -a -q)
+# build image
+docker build -t portfolio-yann .
+# run
+docker run -d -v /etc/letsencrypt/live/ypicard.dev/fullchain.pem:/etc/letsencrypt/live/ypicard.dev/fullchain.pem -v /etc/letsencrypt/live/ypicard.dev/privkey.pem:/etc/letsencrypt/live/ypicard.dev/privkey.pem -e NODE_ENV=production -p 80:8080 -p 443:8443 -t portfolio-yann
